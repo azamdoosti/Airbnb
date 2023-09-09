1805ab51c307ed1456903470334f88a1ad24c0b4
doosti <a.doosti1@gmail.com>
2023-09-09 12:58:04 +0430

    Navbar

---
+++ components/navbar/Search.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='
+    border-[1px]
+    w-full
+    md:w-auto
+    rounded-full
+    shadow-sm
+    hover:shadow-md
+    transition
+    cursor-pointer
+
+    '>
+      <div className='
+      flex
+      flex-row
+      items-center
+      justify-between
+      px-6
+      '>
+      Anywhere
+      </div>
+    </div>
+  )
+}
+
+export default Search
